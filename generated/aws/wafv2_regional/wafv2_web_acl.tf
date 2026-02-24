cfe97f0f66e66ed6511bc16c857d8e52ca4a3f8b
Phil Bywater <phil.bywater@version1.com>
2026-02-24 17:38:03 +0000

    fixed some missing parameters

--- generated/aws/wafv2_regional/wafv2_web_acl.tf
+++ generated/aws/wafv2_regional/wafv2_web_acl.tf
@@ -2,9 +2,17 @@ resource "aws_wafv2_web_acl" "tfer--dsit-litellm-main-alb-waf_d5c33f20" {
   description = "Web application firewall for the application load balancer."
   name        = "dsit-litellm-main-alb-waf"
 
+  default_action {
+    allow {}
+  }
+
   rule {
     name     = "AWS-AWSManagedRulesAmazonIpReputationList"
     priority = "2"
+
+    override_action {
+      none {}
+    }
 
     statement {
       managed_rule_group_statement {
@@ -24,96 +32,166 @@ resource "aws_wafv2_web_acl" "tfer--dsit-litellm-main-alb-waf_d5c33f20" {
     name     = "AWS-AWSManagedRulesCommonRuleSet"
     priority = "0"
 
+    override_action {
+      none {}
+    }
+
     statement {
       managed_rule_group_statement {
         name = "AWSManagedRulesCommonRuleSet"
 
         rule_action_override {
           name = "CrossSiteScripting_BODY"
+          action_to_use {
+            count {}
+          }
         }
 
         rule_action_override {
           name = "CrossSiteScripting_COOKIE"
+          action_to_use {
+            count {}
+          }
         }
 
         rule_action_override {
           name = "CrossSiteScripting_QUERYARGUMENTS"
+          action_to_use {
+            count {}
+          }
         }
 
         rule_action_override {
           name = "CrossSiteScripting_URIPATH"
+          action_to_use {
+            count {}
+          }
         }
 
         rule_action_override {
           name = "EC2MetaDataSSRF_BODY"
+          action_to_use {
+            count {}
+          }
         }
 
         rule_action_override {
           name = "EC2MetaDataSSRF_COOKIE"
+          action_to_use {
+            count {}
+          }
         }
 
         rule_action_override {
           name = "EC2MetaDataSSRF_QUERYARGUMENTS"
+          action_to_use {
+            count {}
+          }
         }
 
         rule_action_override {
           name = "EC2MetaDataSSRF_URIPATH"
+          action_to_use {
+            count {}
+          }
         }
 
         rule_action_override {
           name = "GenericLFI_BODY"
+          action_to_use {
+            count {}
+          }
         }
 
         rule_action_override {
           name = "GenericLFI_QUERYARGUMENTS"
+          action_to_use {
+            count {}
+          }
         }
 
         rule_action_override {
           name = "GenericLFI_URIPATH"
+          action_to_use {
+            count {}
+          }
         }
 
         rule_action_override {
           name = "GenericRFI_BODY"
+          action_to_use {
+            count {}
+          }
         }
 
         rule_action_override {
           name = "GenericRFI_QUERYARGUMENTS"
+          action_to_use {
+            count {}
+          }
         }
 
         rule_action_override {
           name = "GenericRFI_URIPATH"
+          action_to_use {
+            count {}
+          }
         }
 
         rule_action_override {
           name = "NoUserAgent_HEADER"
+          action_to_use {
+            count {}
+          }
         }
 
         rule_action_override {
           name = "RestrictedExtensions_QUERYARGUMENTS"
+          action_to_use {
+            count {}
+          }
         }
 
         rule_action_override {
           name = "RestrictedExtensions_URIPATH"
+          action_to_use {
+            count {}
+          }
         }
 
         rule_action_override {
           name = "SizeRestrictions_BODY"
+          action_to_use {
+            count {}
+          }
         }
 
         rule_action_override {
           name = "SizeRestrictions_Cookie_HEADER"
+          action_to_use {
+            count {}
+          }
         }
 
         rule_action_override {
           name = "SizeRestrictions_QUERYSTRING"
+          action_to_use {
+            count {}
+          }
         }
 
         rule_action_override {
           name = "SizeRestrictions_URIPATH"
+          action_to_use {
+            count {}
+          }
         }
 
         rule_action_override {
           name = "UserAgent_BadBots_HEADER"
+          action_to_use {
+            count {}
+          }
         }
 
         vendor_name = "AWS"
@@ -130,6 +208,10 @@ resource "aws_wafv2_web_acl" "tfer--dsit-litellm-main-alb-waf_d5c33f20" {
   rule {
     name     = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
     priority = "1"
+
+    override_action {
+      none {}
+    }
 
     statement {
       managed_rule_group_statement {
@@ -148,6 +230,10 @@ resource "aws_wafv2_web_acl" "tfer--dsit-litellm-main-alb-waf_d5c33f20" {
   rule {
     name     = "AWS-AWSManagedRulesSQLiRuleSet"
     priority = "3"
+
+    override_action {
+      none {}
+    }
 
     statement {
       managed_rule_group_statement {
